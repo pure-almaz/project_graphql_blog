7adef3a0f138e906adee286a0bb1ec3f90157c69
Philip Oyedoyin <maritine600@gmail.com>
2024-12-09 11:06:17 +0100

    author padding

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { graphCMSImageLoader } from '../util';
 
 const Author = ({ author }) => (
-  <div className="text-center mt-20 mb-8 p-12 relative rounded-lg bg-black bg-opacity-20">
+  <div className="text-center mt-20 mb-8 p-6 sm:p-3 relative rounded-lg bg-black bg-opacity-20">
     <div className="absolute left-0 right-0 -top-14">
       <Image
         unoptimized
@@ -20,5 +20,6 @@ const Author = ({ author }) => (
     <p className="text-white text-ls">{author.bio}</p>
   </div>
 );
+
 
 export default Author;
