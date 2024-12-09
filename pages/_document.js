131e2f77010a50f01629ec34999857ed0ca3902c
Philip Oyedoyin <maritine600@gmail.com>
2024-12-09 21:45:06 +0100

    added analytics

--- pages/_document.js
+++ pages/_document.js
@@ -12,17 +12,17 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" href="/favicon.ico" />
           {/*Google Analytics*/}
-          {/* <script async src="https://www.googletagmanager.com/gtag/js?id=G-620ZDM9WKS"></script>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-9RXKW2WV4P"></script>
           <script
             dangerouslySetInnerHTML={{
               __html: `
                 window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
-                gtag('config', 'G-620ZDM9WKS');
+                gtag('config', 'G-9RXKW2WV4P');
               `,
             }}
-          /> */}
+          />
         </Head>
         <body>
           <Main />
