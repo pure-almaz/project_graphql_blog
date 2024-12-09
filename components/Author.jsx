0627a445c3e892dd079714a457633b0a1f175a11
Philip Oyedoyin <maritine600@gmail.com>
2024-12-09 11:07:57 +0100

    same change

--- components/Author.jsx
+++ components/Author.jsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { graphCMSImageLoader } from '../util';
 
 const Author = ({ author }) => (
-  <div className="text-center mt-20 mb-8 p-6 sm:p-3 relative rounded-lg bg-black bg-opacity-20">
+  <div className="text-center mt-20 mb-8 p-3 sm:p-6 relative rounded-lg bg-black bg-opacity-20">
     <div className="absolute left-0 right-0 -top-14">
       <Image
         unoptimized
