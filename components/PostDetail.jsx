6059f9804f83eefb3536ffe8a32001a0930e7e2f
Philip Oyedoyin <maritine600@gmail.com>
2024-12-12 15:23:27 +0100

    fix multiple line error

--- components/PostDetail.jsx
+++ components/PostDetail.jsx
@@ -147,7 +147,6 @@ const PostDetail = ({ post }) => {
     }
   };
   
-
   return (
     <>
       <div style={{backgroundColor:"white"}} className="bg-white shadow-lg rounded-lg lg:p-8 pb-12 mb-8">
