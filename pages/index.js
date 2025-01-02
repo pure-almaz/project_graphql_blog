c8b1296b264ab36c565e53ad25525e3d1760be26
Philip Oyedoyin <maritine600@gmail.com>
2025-01-02 23:10:11 +0100

    add links to mobile

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,36 @@
 import { FeaturedPosts } from '../sections/index';
 import { PostCard, Categories, PostWidget } from '../components';
-import { getPosts } from '../services';
+import { getCategories, getPosts } from '../services';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 //added a new page
 
 export default function Home({ posts }) {
+  const [linkClick, setLinkClick] = useState(false);
+  const [categories, setCategories] = useState([]);
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChangeComplete = () => {
+      setLinkClick(false);  // Only set to false after visible change
+    };
+
+    // Listen for route change completion
+    router.events.on('routeChangeComplete', handleRouteChangeComplete);
+
+    // Cleanup the event listener on unmount
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChangeComplete);
+    };
+  }, [router, linkClick]); 
+
+  useEffect(() => {
+    getCategories().then((newCategories) => {
+      setCategories(newCategories);
+    });
+  }, []);
 
   return (
     <>
@@ -18,6 +43,15 @@ export default function Home({ posts }) {
       <div className="hidden md:block">
         <FeaturedPosts />
       </div>
+      <div className="float-left md:hidden">
+      {categories.map((category, index) => (
+        <Link key={index} href={`/category/${category.slug}`} passHref>
+          <span onClick={() => setLinkClick(true)} className="md:float-left mt-2 align-middle text-[#35185A] ml-4 font-semibold cursor-pointer">
+            {!linkClick? category.name : "Loading..."}
+          </span>
+        </Link>
+      ))}
+    </div>
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
         <div className="lg:col-span-8 col-span-1">
           {posts.map((post, index) => (
