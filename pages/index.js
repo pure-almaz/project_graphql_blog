86f02115e8556a82ea2a268321284ae6f993c7c7
Philip Oyedoyin <maritine600@gmail.com>
2025-01-03 15:26:01 +0100

    removed featured posts

--- pages/index.js
+++ pages/index.js
@@ -41,10 +41,10 @@ export default function Home({ posts }) {
     <meta name="description" content="Discover expert insights on SEO and web development from Skiie's top 3% professionals. Join us in making a difference with every read." />
     </Head>
       <div className="container mx-auto px-10 mb-8 md: px-2 mb-4 sm: px-1 mb-2">
-      <div className="hidden md:block">
+      {/* <div className="hidden md:block">
         <FeaturedPosts />
-      </div>
-      <div className="float-left md:hidden">
+      </div> */}
+      <div className="mb-5 md:hidden">
       {categories.map((category, index) => (
         <Link key={index} href={`/category/${category.slug}`} passHref>
           <span onClick={() => setLinkClick(true)} className="md:float-left mt-2 align-middle text-[#35185A] ml-4 font-semibold cursor-pointer">
--- sections/FeaturedPosts.jsx
+++ sections/FeaturedPosts.jsx
@@ -37,7 +37,7 @@ const FeaturedPosts = () => {
 
   const customLeftArrow = (
     <div style={{backgroundColor:"#35185A"}} className="absolute arrow-btn left-0 text-center py-3 cursor-pointer bg-[#35185A] rounded-full">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M10 19l-7-7m0 0l7-7m-7 7h18" />
       </svg>
     </div>
@@ -45,14 +45,14 @@ const FeaturedPosts = () => {
 
   const customRightArrow = (
     <div style={{backgroundColor:"#35185A"}} className="absolute arrow-btn right-0 text-center py-3 cursor-pointer bg-[#35185A] rounded-full">
-      <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+      <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 text-white w-full" fill="none" viewBox="0 0 24 24" stroke="currentColor">
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M14 5l7 7m0 0l-7 7m7-7H3" />
       </svg>
     </div>
   );
 
   return (
-    <div className="mb-8 w-full">
+    <div className="relative mb-8 w-full">
     <Carousel
       infinite
       customLeftArrow={customLeftArrow}
