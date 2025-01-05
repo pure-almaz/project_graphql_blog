40df44d0d1ed80b78d2e53307312649e5e4127d1
Philip Oyedoyin <maritine600@gmail.com>
2025-01-05 08:25:32 +0100

    rebuild

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-module.exports = {
-  mode: 'jit', // Add this line
-  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // Use 'content' instead of 'purge'
-  theme: {
-    extend: {
-      spacing: {
-        '2/3': '66.666667%',
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+module.exports = {
+  mode: 'jit', // Add this line
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'], // Use 'content' instead of 'purge'
+  theme: {
+    extend: {
+      spacing: {
+        '2/3': '66.666667%',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
